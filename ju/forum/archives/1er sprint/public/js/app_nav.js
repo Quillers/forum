48834f4438745d1d6a0a7ecb1cd99c3d9d7c766f
ikodi-julien <jupellin39@gmail.com>
2021-01-25 14:48:14 +0100

    Déménagement de mes données

--- ju/forum/archives/1er sprint/public/js/app_nav.js
+++
@@ -1,9 +0,0 @@
-import { Topic } from './class/Topic.js'
-
-const topic = new Topic()
-
-document.addEventListener('DOMContentLoaded', () => {
-  topic.createTopicName()
-  topic.createAllMessagesContainer()
-  topic.createForm()
-})
