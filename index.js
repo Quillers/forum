b615916edb673c2deb876583c2f9bf26cd875c05
ikodi-julien <jupellin39@gmail.com>
2021-01-28 21:38:26 +0100

    Fait les routes '/' et 'createAccount', avec la session !

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-/* 
-** include express
-*/
+/*
+ ** include express
+ */
 const express = require('express');
-const router = require('./my_modules/router')
+const router = require('./my_modules/router');
 
 const app = express();
-
+const session = require('express-session');
 
 // user urlencoded to get data from post
 
@@ -13,9 +13,9 @@ app.use(express.urlencoded({ extended: false }));
 
 const port = 3000;
 
-/* 
-** set functionnalities
-*/
+/*
+ ** set functionnalities
+ */
 // set views directory
 app.set('views', './views');
 //set template engine to ejs
@@ -23,16 +23,27 @@ app.set('view engine', 'ejs');
 //set the static file directory
 app.use(express.static('public'));
 
+// On ouvre la session
+app.use(
+  session({
+    cookie: { maxAge: 60000 },
+    secret: 'quillers',
+    session: {
+      loggedIn: false,
+    },
+    resave: true,
+    saveUninitialized: false,
+  })
+);
+
 // We create a Global variable where we store the categories list (that s ok bc it will only be modified by the admins)
 app.locals.categories = require('./my_modules/database/categories.json');
 
 //use the router
+app.use(router);
 
-app.use(router)
-
-
-/* 
-** start server and listen to port
-*/
+/*
+ ** start server and listen to port
+ */
 
 app.listen(port, console.log('server started at port: ', port));
--- my_modules/router.js
+++ my_modules/router.js
@@ -1,6 +1,6 @@
-/* 
-** Include express
-*/
+/*
+ ** Include express
+ */
 const express = require('express');
 const path = require('path');
 
@@ -8,43 +8,77 @@ const path = require('path');
 
 const postMW = require('./middlewares/postMW');
 const getMW = require('./middlewares/getMW');
+const connexionMW = require('./middlewares/connexionMW');
 
-/* 
-** set the router
-*/
+/*
+ ** set the router
+ */
 
 const router = express.Router();
 
-/* 
-** GET REQUESTS
-*/
-
+/*
+ ** GET REQUESTS
+ */
 
 router.get('/', (request, response) => {
-    // Renvoi la page d'accueil avec les catégories en dynamique
-    // dans la nav, l'objet categories.json est stocké dans 
-    // app.locals.categories depuis index.js
-    response.render('index');
+  // Renvoi la page d'accueil avec les catégories en dynamique
+  // dans la nav, l'objet categories.json est stocké dans
+  // app.locals.categories depuis index.js
+  //Vérification de la session
+  (request.session.info = 'vide, on test'),
+    response.render('index', {
+      info: request.session.info,
+      loggedIn: request.session.loggedIn,
+    });
 });
 
-router.get('/categories/:categoryName', getMW.getCategoryId, getMW.fetchTopicsDB, (request, response) => {
+router.get(
+  '/categories/:categoryName',
+  getMW.getCategoryId,
+  getMW.fetchTopicsDB,
+  (request, response) => {
     // See middlewares for more informations
-    response.render('category', { topics: response.locals.topics, categoryName: request.params.categoryName });
-});
+    response.render('category', {
+      topics: response.locals.topics,
+      categoryName: request.params.categoryName,
+    });
+  }
+);
 
-router.get('/topics/:categoryName/:topicName',  getMW.fetchMessagesDB, (request, response) => {
+router.get(
+  '/topics/:categoryName/:topicName',
+  getMW.fetchMessagesDB,
+  (request, response) => {
     // idem
-    response.render('topic', {  messages: response.locals.messages });
-});
+    response.render('topic', { messages: response.locals.messages });
+  }
+);
 
-/* 
-** POST REQUESTS
-*/
+// CONNEXION
+router.get('/connexion', connexionMW.renderConnexionForm);
+router.get('/connexion/createAccount', connexionMW.renderCreateAccountForm);
 
-router.post('/topics/:categoryName/:topicName', postMW.validateResponseForm, postMW.insertMessageDB, (request, response) => {
-   //TODO
-   //
-    response.redirect(`/topics/${request.params.categoryName}/${request.params.topicName}`);
-});
+/*
+ ** POST REQUESTS
+ */
+
+router.post(
+  '/topics/:categoryName/:topicName',
+
+  postMW.validateResponseForm,
+
+  postMW.insertMessageDB,
+  (request, response) => {
+    //TODO
+    //
+    response.redirect(
+      `/topics/${request.params.categoryName}/${request.params.topicName}`
+    );
+  }
+);
+
+// CONNEXION
+
+router.post('/postConnexion/:pass', connexionMW.selectRoute);
 
 module.exports = router;
