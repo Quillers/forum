f53e7928a7e95fbf91bfc0eb761589a81a41f2e1
ikodi-julien <jupellin39@gmail.com>
2021-02-09 00:26:23 +0100

    Fait du scss sur stdLogin et créer des partials pour header et footer

--- app/connexion/model/connexionDB.js
+++ app/connexion/model/connexionDB.js
@@ -6,24 +6,33 @@ const connexionDB = {
 
   getUser: function(pseudo, callback) {
 
-    const query = `SELECT * FROM users 
-    WHERE pseudo = '${pseudo}';`;
+    const query = {
+      text: `SELECT * FROM users 
+    WHERE pseudo=$1;`,
+      values: [pseudo]
+    }
 
     client.query(query, callback)
   },
 
   getPseudo: function(pseudo, callback) {
 
-    const query = `SELECT * FROM users 
-    WHERE pseudo='${pseudo}';`;
+    const query = {
+      text: `SELECT * FROM users 
+    WHERE pseudo=$1;`,
+      values: [pseudo]
+    }
 
     client.query(query, callback)
   },
 
   insertProfil: function(pseudo, hashedPass, email, callback) {
 
-    const query = `INSERT INTO users (pseudo, password, email, status)
-    VALUES ('${pseudo}', '${hashedPass}', '${email}', 'stdUser');`;
+    const query = {
+      text: `INSERT INTO users (pseudo, password, email, status)
+    VALUES ('${pseudo}', '${hashedPass}', '${email}', 'stdUser');`,
+      values: [pseudo, hashedPass, email, 'stdUser']
+    };
 
     client.query(query, callback)
   },
